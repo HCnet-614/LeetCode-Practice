cb41f51907ad6f4a515d000cd6f0320b22a8e4e6
Cheng <62643483+HCnet-614@users.noreply.github.com>
2021-12-11 23:19:25 +0800

    Reverse Linked List

---
+++ 206.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pre = head       # 申请节点 / Application node
+        cur = None
+        while(pre != None):
+            t = pre.next   # 记录下一个节点 / Record next node
+            pre.next = cur  # 指到前一个节点 / Refers to the previous node
+            cur = pre   # 两个节点都前进一位 / Both nodes advance one bit
+            pre = t
+        return cur
