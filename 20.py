60a0be8dfcfec4b7f2f63d51c108aa0e6d356365
Cheng <62643483+HCnet-614@users.noreply.github.com>
2021-12-18 10:49:15 +0800

    valid-parentheses

--- 20.py
+++ 20.py
@@ -3,12 +3,13 @@ class Solution:
         stack = []
         for i in s:
             if i == '(' or i == '{' or i == '[':
-                stack.append(i)
+                stack.append(i)   #把左括号添加至栈/Add left parenthesis to stack 
             else:
                 if len(stack) == 0:
                     return False
                 else:
-                    temp = stack.pop()
+                    temp = stack.pop()  #取栈顶元素/Get stack top element 
+                    #匹配左、右括号/Match left and right parentheses
                     if i == '}':
                         if temp!='{':
                             return False
